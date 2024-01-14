74a26622e414266dd3e485fa30b0b87e1cfffac6
Romain Laurence <mw3pumat8@gmail.com>
2024-01-14 16:02:15 +0100

    ajout logique de login avec la requête

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,4 +1,39 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, type Ref } from 'vue'
+
+interface Credentials {
+  email: string
+  password: string
+  remember: boolean
+}
+
+const credentials: Ref<Credentials> = ref({
+  email: '',
+  password: '',
+  remember: false
+})
+
+const login = async () => {
+  try {
+    const request = await fetch(`api/auth/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(credentials.value)
+    })
+
+    const response = await request.json()
+    if (response.statut === 'error') throw new Error(response.msg)
+    if (response.statut === 'ok') {
+      localStorage.setItem('token', response.token)
+      console.log(localStorage.getItem('token'))
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+</script>
 
 <template>
   <main class="w-full h-fit p-4 pt-12">
@@ -7,7 +42,7 @@
         <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
           Connectez-vous
         </h1>
-        <form class="space-y-4 md:space-y-6" action="#">
+        <div class="space-y-4 md:space-y-6">
           <div>
             <label for="email" class="block mb-2 text-sm font-medium text-gray-900"
               >Adresse e-mail</label
@@ -15,6 +50,7 @@
             <input
               type="email"
               name="email"
+              v-model="credentials.email"
               id="email"
               class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
               placeholder="nom.prenom@nurse.care"
@@ -28,6 +64,7 @@
               type="password"
               name="password"
               id="password"
+              v-model="credentials.password"
               placeholder="••••••••"
               class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
             />
@@ -39,6 +76,7 @@
                   id="remember"
                   aria-describedby="remember"
                   type="checkbox"
+                  v-model="credentials.remember"
                   class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
                 />
               </div>
@@ -48,12 +86,13 @@
             </div>
           </div>
           <button
-            type="submit"
+            type="button"
+            v-on:click="login()"
             class="w-full text-white bg-[#f6a2d2] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
           >
             Se connecter
           </button>
-        </form>
+        </div>
       </div>
     </div>
   </main>
