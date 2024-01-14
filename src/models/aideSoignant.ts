002145b1fcda749c3404a2e128f2abfda2e7ffbc
Romain Laurence <mw3pumat8@gmail.com>
2024-01-14 21:09:07 +0100

    ajout du modele aide soignant

---
+++ src/models/aideSoignant.ts
@@ -0,0 +1,16 @@
+export default interface AideSoignant {
+  id: number
+  nom: string
+  prenom: string
+  email: string
+  password: string
+  telephone: string
+  adresse: string
+  ville: string
+  codePostal: string
+  pays: string
+  dateNaissance: string
+  dateEmbauche: string
+  role: string
+  token: string
+}
--- src/views/espaces/aideSoignant/HomeView.vue
+++ src/views/espaces/aideSoignant/HomeView.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
 import AppMenu from '/src/components/AppMenu.vue'
+import AideSoignant from '/src/models/aideSoignant'
+import { ref } from 'vue'
+
+const userProfile: ref<AideSoignant> = ref({
+  id: 0,
+  nom: '',
+  prenom: '',
+  email: '',
+  password: '',
+  adresse: '',
+  telephone: '',
+  role: 'aideSoignant'
+})
+
+console.log(userProfile.value)
 </script>
 
 <template>
   <AppMenu />
 
-  <router-view></router-view>
+  <router-view msg="Profil aide soignant"></router-view>
 </template>
 
 <style scoped></style>
