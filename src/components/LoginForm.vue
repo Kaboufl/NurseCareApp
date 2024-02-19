2fa373f79d3f5685871e4e47b4358699ff2d710f
kaboufl <mw3pumat8@gmail.com>
2024-02-19 01:00:40 +0100

    patch form connexion interaction

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -81,7 +81,7 @@ const login = async () => {
 <template>
   <main class="w-full h-fit p-4 pt-12">
     <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
-      <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+      <form @submit.prevent="login()" class="p-6 space-y-4 md:space-y-6 sm:p-8">
         <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
           Connectez-vous
         </h1>
@@ -135,14 +135,13 @@ const login = async () => {
             </div>
           </div> -->
           <button
-            type="button"
-            v-on:click="login()"
+            type="submit"
             class="w-full text-white bg-[#f6a2d2] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
           >
             Se connecter
           </button>
         </div>
-      </div>
+      </form>
     </div>
   </main>
 </template>
