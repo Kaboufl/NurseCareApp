f935fb7855f3af580f12fcb091b9bf4871177ca3
Romain Laurence <r.laurence@webizweb.com>
2024-02-27 14:11:45 +0100

    retrait de page d'acceuil

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,15 +13,7 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'Accueil',
-      component: HomeView
-    },
-    {
-      path: '/login',
       name: 'Connexion',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: LoginForm
     },
     {
